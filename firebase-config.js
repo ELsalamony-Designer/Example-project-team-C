89b41b97d66fa9e96859d4235ad2557f62176a5d
ELsalamony-Designer <mahmoudelsalamony2000@gmail.com>
2025-07-12 23:21:47 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,27 +1,26 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getDatabase } from "firebase/database"; // Import for Realtime Database
+import { getStorage } from "firebase/storage"; // Import for Firebase Storage
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDdLJq7F6r6O9XZTQTIpwdrLR0ZySLGQrY",
-  authDomain: "shkl-1db60.firebaseapp.com",
-  databaseURL: "https://shkl-1db60-default-rtdb.firebaseio.com",
-  projectId: "shkl-1db60",
-  storageBucket: "shkl-1db60.firebasestorage.app",
-  messagingSenderId: "83946018492",
-  appId: "1:83946018492:web:3cfa51fa2ddebee27de2b9",
-  measurementId: "G-PW83MCMQYK"
+  apiKey: "AIzaSyDpT-XyPKNPKGetKYl_qDPKsdqL_u2SCyk",
+  authDomain: "atr-egclothes-2faeb.firebaseapp.com",
+  databaseURL: "https://atr-egclothes-2faeb-default-rtdb.firebaseio.com",
+  projectId: "atr-egclothes-2faeb",
+  storageBucket: "atr-egclothes-2faeb.firebasestorage.app",
+  messagingSenderId: "455708726120",
+  appId: "1:455708726120:web:a35a5ffbbb8c5e8b0dfcdd",
+  measurementId: "G-99J4WYBTM9"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
-// Export the database instance if you need to use it in other files
-// For Realtime Database, you would typically get it like this:
-import { getDatabase } from "firebase/database";
+// Export the database and storage instances for use in other files
 export const database = getDatabase(app);
+export const storage = getStorage(app); // Export the storage instance
